d6f3a76ae575932c8900b81dd4a30f57695800e6
Phillip Bartschinski <phillip@bartschinski.com>
2021-02-21 22:27:16 +0100

    Add FertilizedSoil block

--- src/main/java/de/gontrix/farmingcompost/common/blocks/FertilizedSoil.java
+++ src/main/java/de/gontrix/farmingcompost/common/blocks/FertilizedSoil.java
@@ -3,11 +3,13 @@ package de.gontrix.farmingcompost.common.blocks;
 import de.gontrix.farmingcompost.FarmingCompost;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
 
 public class FertilizedSoil extends Block {
     public FertilizedSoil() {
-        super(AbstractBlock.Properties.create(Material.EARTH));
+        super(AbstractBlock.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(0.5f, 0.5f).harvestLevel(0).harvestTool(ToolType.SHOVEL));
     }
 
     public static FertilizedSoil getFertilizedSoil() {
