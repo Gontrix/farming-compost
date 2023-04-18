e81ec7b9d9ff3c0b5f7ff68ac1ad1d28d16e0214
Phillip Bartschinski <phillip@bartschinski.com>
2023-04-18 18:10:48 +0200

    Update block behavior

--- src/main/java/de/gontrix/farmingcompost/common/blocks/FertilizedFieldBlock.java
+++ src/main/java/de/gontrix/farmingcompost/common/blocks/FertilizedFieldBlock.java
@@ -2,42 +2,100 @@ package de.gontrix.farmingcompost.common.blocks;
 
 import de.gontrix.farmingcompost.FarmingCompost;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class FertilizedFieldBlock extends Block {
     public static final String NAME = "fertilized_field";
 
+    public static IntegerProperty MOISTURE = BlockStateProperties.MOISTURE;
     protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 15.0D, 16.0D);
 
 
     public FertilizedFieldBlock() {
-        super(BlockBehaviour.Properties.of(Material.DIRT));
+        super(BlockBehaviour.Properties.of(Material.DIRT, MaterialColor.DIRT)
+                .randomTicks()
+                .strength(0.6F)
+                .sound(SoundType.GRAVEL)
+        );
+        this.registerDefaultState(this.stateDefinition.any().setValue(MOISTURE, 0));
         FarmingCompost.LOGGER.info("FarmingCompost FertilizedField");
-//        super(AbstractBlock.Properties.create(Material.EARTH).sound(SoundType.GROUND).hardnessAndResistance(0.5f, 0.5f).harvestLevel(0).harvestTool(ToolType.SHOVEL));
     }
 
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(MOISTURE);
+    }
 
     @Override
     public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
         return SHAPE;
     }
 
-//
-//    @Override
-//    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-//        if (!worldIn.isRemote) {
-//            if (pos.up(1).getY() == fromPos.getY() && worldIn.getBlockState(fromPos).isSolid()) {
-//                worldIn.setBlockState(pos, Blocks.fertilizedSoil.getDefaultState());
-//            }
-//        }
-//
-//        super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
-//    }
+    @Override
+    public void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
+        int i = blockState.getValue(MOISTURE);
 
+        if (!isNearWater(serverLevel, blockPos) && !serverLevel.isRainingAt(blockPos.above())) {
+            if (i > 0) {
+                serverLevel.setBlock(blockPos, blockState.setValue(MOISTURE, Integer.valueOf(i - 1)), 2);
+            } else if (!isUnderCrops(serverLevel, blockPos)) {
+                turnToSoil(blockState, serverLevel, blockPos);
+            }
+
+        } else if (i < 7) {
+            serverLevel.setBlock(blockPos, blockState.setValue(MOISTURE, Integer.valueOf(7)), 2);
+        }
+    }
+
+    private static boolean isUnderCrops(BlockGetter blockGetter, BlockPos blockPos) {
+        BlockState plant = blockGetter.getBlockState(blockPos.above());
+        BlockState state = blockGetter.getBlockState(blockPos);
+
+        return plant.getBlock() instanceof net.minecraftforge.common.IPlantable && state.canSustainPlant(blockGetter, blockPos, Direction.UP, (net.minecraftforge.common.IPlantable)plant.getBlock());
+    }
+
+    private static boolean isNearWater(LevelReader levelReader, BlockPos blockPos) {
+        BlockState state = levelReader.getBlockState(blockPos);
+
+        for(BlockPos blockpos : BlockPos.betweenClosed(
+            blockPos.offset(-4, -1, -4),
+            blockPos.offset(4, 1, 4)
+        )) {
+            if (state.canBeHydrated(levelReader, blockPos, levelReader.getFluidState(blockpos), blockpos)) {
+                return true;
+            }
+        }
+
+        return net.minecraftforge.common.FarmlandWaterManager.hasBlockWaterTicket(levelReader, blockPos);
+    }
+
+    public static void turnToSoil(BlockState blockState, Level level, BlockPos blockPos) {
+        BlockState blockstate = pushEntitiesUp(blockState, Blocks.FERTILIZED_SOIL.get().defaultBlockState(), level, blockPos);
+
+        level.setBlockAndUpdate(blockPos, blockstate);
+        level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of((Entity)null, blockstate));
+    }
+
+    public boolean isPathfindable(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, PathComputationType pathComputationType) {
+        return false;
+    }
 }
--- src/main/java/de/gontrix/farmingcompost/common/blocks/FertilizedSoilBlock.java
+++ src/main/java/de/gontrix/farmingcompost/common/blocks/FertilizedSoilBlock.java
@@ -1,46 +1,52 @@
 package de.gontrix.farmingcompost.common.blocks;
 
 import de.gontrix.farmingcompost.FarmingCompost;
-import de.gontrix.farmingcompost.common.items.FertilizedSoilItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.ToolActions;
 
 public class FertilizedSoilBlock extends Block {
     public static final String NAME = "fertilized_soil";
 
     public FertilizedSoilBlock() {
-        super(Block.Properties.copy(net.minecraft.world.level.block.Blocks.DIRT));
+        super(BlockBehaviour.Properties.of(Material.DIRT, MaterialColor.DIRT)
+                .randomTicks()
+                .strength(0.5F)
+                .sound(SoundType.GRAVEL)
+        );
         FarmingCompost.LOGGER.info("FarmingCompost FertilizedSoil");
-//        super(AbstractBlock.Properties.create(Material.EARTH).sound(SoundType.ROOTED_DIRT).hardnessAndResistance(0.5f, 0.5f).harvestLevel(0).harvestTool(ToolType.SHOVEL));
     }
 
-//    @Override
-//    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-//        FarmingCompost.LOGGER.info("FarmingCompost InteractionResult");
-//        ItemStack held = player.getItemInHand(hand);
-//
-//        if (!world.isClientSide() && held.canPerformAction(ToolActions.HOE_DIG)){
-//            FarmingCompost.LOGGER.info("FarmingCompost canPerformAction");
-////            world.explode(player, pos.getX(), pos.getY(), pos.getZ(), 4.0F, true, Explosion.Mode.DESTROY);
-////            held.shrink(1);
-//            return InteractionResult.SUCCESS;
-//        }
-//
-//        return super.use(state, world, pos, player, hand, hit);
-//    }
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult hit) {
+        FarmingCompost.LOGGER.info("FarmingCompost InteractionResult");
+        if (level.isClientSide()) { FarmingCompost.LOGGER.info("isClientSide"); }
+        ItemStack held = player.getItemInHand(hand);
 
-//    @Override
-//    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-//        if (!worldIn.isRemote) {
-//            if (player.getHeldItem(handIn).getToolTypes().contains(ToolType.HOE)) {
-//                if (!worldIn.getBlockState(pos.up(1)).isSolid()) {
-//                    worldIn.setBlockState(pos, Blocks.fertilizedField.getDefaultState());
-//
-//                    return ActionResultType.SUCCESS;
-//                }
-//            }
-//        }
-//
-//        return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
-//    }
+        if (!level.isClientSide() && hand == InteractionHand.MAIN_HAND && held.canPerformAction(ToolActions.HOE_DIG)){
+            FarmingCompost.LOGGER.info("FarmingCompost canPerformAction");
+            BlockState blockstate = pushEntitiesUp(blockState, Blocks.FERTILIZED_FIELD.get().defaultBlockState(), level, blockPos);
+
+            level.setBlockAndUpdate(blockPos, blockstate);
+            level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of((Entity)null, blockstate));
+
+            return InteractionResult.SUCCESS;
+        }
+
+        return super.use(blockState, level, blockPos, player, hand, hit);
+    }
+
 }
